4932b4e8df433c350a0a870300f698b26f44b2ff
Mariam Ehab Codes <64708267+MariamEhabS@users.noreply.github.com>
2021-07-09 22:42:16 +0200

    adding another page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors') 
+const { response } = require('express')
 const PORT = 8000
 
 app.use(cors)
@@ -8,6 +9,10 @@ app.use(cors)
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+app.get('/feed'), (req, res) => {
+    res.json(__dirname + '/public/feed.html')
+}
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`server running on port ${PORT}`)
