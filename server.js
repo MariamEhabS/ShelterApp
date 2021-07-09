36c187b22475a1cc5f57201c95ee41e56e0050c2
Mariam Ehab Codes <64708267+MariamEhabS@users.noreply.github.com>
2021-07-09 20:47:20 +0200

    adding all files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors') 
+const PORT = 8000
+
+app.use(cors)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
